bd072a6a4c502be1b314f770af7ac43e57aba4dd
Mohamed Chamseddine Kadri <mohamedchamseddinekadri@gmail.com>
2024-10-02 22:38:17 -0500

    persistance jpa hybernate project

---
+++ j-persistance-api/src/main/java/com/example/jpersistanceapi/JPersistanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpersistanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPersistanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPersistanceApiApplication.class, args);
+	}
+
+}
---
+++ j-persistance-api/src/test/java/com/example/jpersistanceapi/JPersistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpersistanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPersistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
